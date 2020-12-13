a4382a33c361b360b415e22fa9c3d85485627e2e
MaximusXYZ <61899298+MaximusXYZ@users.noreply.github.com>
2020-12-13 13:22:20 +0800

    Chester - Updated v0.0.1
    - Added ThemeInterface

---
+++ Chester/Extensions/Data.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+public static class Data
+{
+    public class Border
+    {
+        public SolidColorBrush Background;
+        public SolidColorBrush BorderBrush;
+        public Thickness BorderThickness;
+        public double Opacity;
+    }
+
+    public class MainWindow
+    {
+        public string Title;
+        public Border Background;
+    }
+
+    public class Main
+    {
+        public MainWindow MainWindow;
+    }
+
+    public static void ApplyBorder(System.Windows.Controls.Border Border, Border _Border)
+    {
+        Border.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom(_Border.Background.ToString()));
+        Border.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom(_Border.BorderBrush.ToString()));
+        Border.BorderThickness = (_Border.BorderThickness);
+        Border.Opacity = (_Border.Opacity);
+    }
+
+    public static Main Default()
+    {
+        var Main = new Main
+        {
+            MainWindow = new MainWindow
+            {
+                Title = "Chester - MainWindow",
+                Background = new Border
+                {
+                    Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#141414")),
+                    BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#ffffff00")),
+                    BorderThickness = new Thickness(0),
+                    Opacity = 1
+                }
+            }
+        };
+
+        return Main;
+    } 
+}
---
+++ Chester/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+
+namespace Chester
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("user-data.json"))
+            {
+                File.WriteAllText("user-data.json", JsonConvert.SerializeObject(Data.Default(), Formatting.Indented));
+            }
+            else
+            {
+                try
+                {
+                    Manager.GetMain = JsonConvert.DeserializeObject<Data.Main>(File.ReadAllText("user-data.json"));
+                    var Main = Manager.GetMain.MainWindow;
+
+                    Data.ApplyBorder(Background, Main.Background);
+                }
+                catch (Exception ex)
+                {
+                    var Result = MessageBox.Show(
+                        "Everest has encountered an error while loading Themes. Would you like to copy the exception?",
+                        "Everest - Exception",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Error);
+
+                    if (Result != MessageBoxResult.Yes) return;
+                    else { Clipboard.SetText(ex.ToString()); Environment.Exit(0); }
+                }
+            }
+        }
+    }
+}
